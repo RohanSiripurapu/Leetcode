679213446c678e472b6c58f3c5063ffa0aa48433
rohansiri01 <167692992+rohansiri01@users.noreply.github.com>
2026-08-03 09:30:57 +0530

    Time: 10 ms (36.42%) | Memory: 49 MB (7.61%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int result[]=new int[n];
         for(int i=0;i<n;i++){
-            result[i]=nums[i]*nums[i];
+            result[i]=(int) Math.pow(nums[i],2);
         }
         Arrays.sort(result);
         return result;
