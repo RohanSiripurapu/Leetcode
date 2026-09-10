acb79774b78ce60f525a23901b05272ce6922484
Rohan Siripurapu <167692992+RohanSiripurapu@users.noreply.github.com>
2026-09-10 17:45:22 +0530

    Time: 1 ms (99.89%) | Memory: 45.3 MB (49.35%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,25 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] map = new int[10001];
+        
+        int[] nextGreater = new int[10001];
+        
+       
         int[] stack = new int[nums2.length];
         int top = -1;
 
         for (int num : nums2) {
+         
             while (top >= 0 && stack[top] < num) {
-                map[stack[top--]] = num;
+                nextGreater[stack[top--]] = num;
             }
             stack[++top] = num;
         }
 
         while (top >= 0) {
-            map[stack[top--]] = -1;
+            nextGreater[stack[top--]] = -1;
         }
 
-        int[] result = new int[nums1.length];
+        int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            result[i] = map[nums1[i]] == 0 ? -1 : map[nums1[i]];
+            ans[i] = nextGreater[nums1[i]];
         }
 
-        return result;
+        return ans;
     }
 }
