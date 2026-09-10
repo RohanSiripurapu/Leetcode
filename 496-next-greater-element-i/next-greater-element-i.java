55618f455baee7b72a5854b856f6301d9296a618
Rohan Siripurapu <167692992+RohanSiripurapu@users.noreply.github.com>
2026-09-10 17:44:38 +0530

    Time: 1 ms (99.89%) | Memory: 45.5 MB (21.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,35 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] map = new int[10001];
+        int[] stack = new int[nums2.length];
+        int top = -1;
+
+        for (int num : nums2) {
+            while (top >= 0 && stack[top] < num) {
+                map[stack[top--]] = num;
+            }
+            stack[++top] = num;
+        }
+
+        while (top >= 0) {
+            map[stack[top--]] = -1;
+        }
 
         int[] result = new int[nums1.length];
-
         for (int i = 0; i < nums1.length; i++) {
-
-            Stack<Integer> stack = new Stack<>();
-            boolean found = false;
-
-            for (int j = 0; j < nums2.length; j++) {
-
-                if (nums2[j] == nums1[i]) {
-                    stack.push(nums2[j]);
-
-                    for (int k = j + 1; k < nums2.length; k++) {
-                        if (nums2[k] > stack.peek()) {
-                            result[i] = nums2[k];
-                            found = true;
-                            break;
-                        }
-                    }
-
-                    if (!found) {
-                        result[i] = -1;
-                    }
-
-                    break;
-                }
-            }
+            result[i] = map[nums1[i]] == 0 ? -1 : map[nums1[i]];
         }
 
         return result;
